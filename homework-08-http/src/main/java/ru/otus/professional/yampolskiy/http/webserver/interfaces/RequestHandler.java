75f9f643a6e41bf6c1fb0b4e6f6f1b65bf522c0b
mrTwice <yampolskiy86@gmail.com>
2024-11-24 19:03:31 +0300

    finished homework 08

---
+++ homework-08-http/src/main/java/ru/otus/professional/yampolskiy/http/webserver/interfaces/RequestHandler.java
@@ -0,0 +1,8 @@
+package ru.otus.professional.yampolskiy.http.webserver.interfaces;
+
+import ru.otus.professional.yampolskiy.http.webserver.http.HttpRequest;
+import ru.otus.professional.yampolskiy.http.webserver.http.HttpResponse;
+
+public interface RequestHandler {
+    HttpResponse execute(HttpRequest request) throws Exception;
+}
