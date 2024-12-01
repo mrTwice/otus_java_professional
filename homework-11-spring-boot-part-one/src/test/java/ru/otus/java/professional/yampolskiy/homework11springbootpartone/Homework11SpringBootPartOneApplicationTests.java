9cd41eaf4b19baea2245eb700082de160140557c
mrTwice <yampolskiy86@gmail.com>
2024-12-01 10:27:18 +0300

    create homework project add README.md

---
+++ homework-11-spring-boot-part-one/src/test/java/ru/otus/java/professional/yampolskiy/homework11springbootpartone/Homework11SpringBootPartOneApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.java.professional.yampolskiy.homework11springbootpartone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework11SpringBootPartOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
