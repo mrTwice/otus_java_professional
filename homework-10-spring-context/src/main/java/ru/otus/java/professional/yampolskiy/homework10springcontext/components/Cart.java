fc88ce85427eb3fecdec5b2926bdf7eb20908d03
mrTwice <yampolskiy86@gmail.com>
2024-12-01 10:14:23 +0300

    fix after review

--- homework-10-spring-context/src/main/java/ru/otus/java/professional/yampolskiy/homework10springcontext/components/Cart.java
+++ homework-10-spring-context/src/main/java/ru/otus/java/professional/yampolskiy/homework10springcontext/components/Cart.java
@@ -1,5 +1,6 @@
 package ru.otus.java.professional.yampolskiy.homework10springcontext.components;
 
+import jakarta.annotation.PostConstruct;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.Scope;
@@ -13,7 +14,12 @@ import java.util.List;
 @Scope("prototype")
 public class Cart {
     private static final Logger logger = LogManager.getLogger(Cart.class);
-    private final List<Product> items = new ArrayList<>();
+    private List<Product> items;
+
+    @PostConstruct
+    public void init() {
+        items = new ArrayList<>();
+    }
 
     public void addProduct(Product product) {
         items.add(product);
