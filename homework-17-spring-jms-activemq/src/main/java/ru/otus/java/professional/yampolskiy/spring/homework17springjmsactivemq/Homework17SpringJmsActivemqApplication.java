f373e3a71e4d8694f57be70d4fbc8ba7e19caaf6
mrTwice <yampolskiy86@gmail.com>
2025-03-09 15:19:31 +0300

    add homework-17 module

---
+++ homework-17-spring-jms-activemq/src/main/java/ru/otus/java/professional/yampolskiy/spring/homework17springjmsactivemq/Homework17SpringJmsActivemqApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.java.professional.yampolskiy.spring.homework17springjmsactivemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework17SpringJmsActivemqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework17SpringJmsActivemqApplication.class, args);
+    }
+
+}
---
+++ homework-17-spring-jms-activemq/src/test/java/ru/otus/java/professional/yampolskiy/spring/homework17springjmsactivemq/Homework17SpringJmsActivemqApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.java.professional.yampolskiy.spring.homework17springjmsactivemq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework17SpringJmsActivemqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
